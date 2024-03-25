edf7e6a0d2e7feaa390f75c07b79632e8b957f04
gangagamit <gangagamit350@gmail.com>
2024-03-25 12:21:18 +0530

    25-3-2024

---
+++ program/p15.js
@@ -0,0 +1,14 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+
+// Example 1:
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+// One value was passed to the function so it should return 1.
+
+const argumentsLength = function(...args) {
+    return args.length;
+ };
